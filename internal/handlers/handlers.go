c2645c067cd4083314f9460ca39968f30877107f
Фокин Иван Игоревич <nahof90@gmail.com>
2025-07-13 21:24:16 +0300

    First commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,74 @@
 package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "Failed to load HTML", http.StatusInternalServerError)
+		log.Println("Error reading index.html:", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(file)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Error parsing form", http.StatusInternalServerError)
+		log.Println("ParseMultipartForm error:", err)
+		return
+	}
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Failed to get file from form", http.StatusInternalServerError)
+		log.Println("FormFile error:", err)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		log.Println("ReadAll error:", err)
+		return
+	}
+
+	converted, err := service.Convert(string(data))
+	if err != nil {
+		http.Error(w, "Data conversion error", http.StatusInternalServerError)
+		log.Println("Convert error:", err)
+		return
+	}
+
+	filename := time.Now().UTC().Format("20060102150405") + filepath.Ext(header.Filename)
+	f, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, "Failed to create file", http.StatusInternalServerError)
+		log.Println("Create file error", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(converted)
+	if err != nil {
+		http.Error(w, "Error writing to file", http.StatusInternalServerError)
+		log.Println("WriteString error:", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(converted))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,35 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(data string) (string, error) {
+	data = strings.TrimSpace(data)
+	if isMorse(data) {
+		return morse.ToText(data), nil
+	}
+
+	return morse.ToMorse(data), nil
+}
+
+func isMorse(s string) bool {
+
+	for _, r := range s {
+		if r != '.' && r != '-' && r != ' ' {
+			return false
+		}
+	}
+
+	hasMorseChar := false
+	for _, r := range s {
+		if r == '.' || r == '-' {
+			hasMorseChar = true
+			break
+		}
+	}
+
+	return hasMorseChar
+}
